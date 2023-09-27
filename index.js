051a2fbd7cb1ad29057c42828ee02094adabe1f6
unknown <dubemobiwon@gmail.com>
2023-09-27 09:30:09 -0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const rateLimit = require("express-rate-limit");
 const AppError = require("./src/utils/appError");
 const globalErrorHandler = require("./src/controllers/error-controller");
 const userRouter = require("./src/routes/user-routes");
@@ -11,14 +17,33 @@ const voteRouter = require("./src/routes/vote-routes");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
 
-// app.use("/api/v1/users", (req, res, next) => {
-//   console.log("Middleware is running");
-//   res.status(200).json({
-//     message: "This Middleware is working properly",
-//   });
-//   next();
-// });
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 100,
+  message:
+    "Too many request from the same IP address. Try again in 1 hour time.",
+});
+app.use("/api", limiter);
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/contestant", contestantRouter);
