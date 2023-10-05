1a01c4a3f71fa149ae73ecbfee5a33009c61f9c9
unknown <dubemobiwon@gmail.com>
2023-10-05 11:10:08 -0700

    update

--- src/controllers/error-controller.js
+++ src/controllers/error-controller.js
@@ -54,13 +54,14 @@ const sendErrorProd = (err, req, res) => {
         message: err.message,
       });
     }
+    console.log(err);
     // B) Programming or other unknown error: don't leak error details
     // 1) Log error
     console.error("ERROR 💥", err);
     // 2) Send generic message
     return res.status(500).json({
       status: "error",
-      message: "Something went very wrong!,"
+      message: "Something went very wrong!,",
     });
   }
 
